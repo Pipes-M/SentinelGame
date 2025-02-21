d2236ab62dcfe4e25e2457c525df7b518a52a524
MaxP <max.piper.uk@gmail.com>
2025-02-21 12:25:32 +0000

    feet normal

--- Assets/Scripts/TestIk.cs
+++ Assets/Scripts/TestIk.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class TestIk : MonoBehaviour
 {
     public float distNewStep = 10f;
     public float legHeightAdjust = 1f;
     public float rayLength = 100f;
+    public GameObject foot;
     public GameObject ikTip;
     private Vector3 firstStep;
     private Vector3 secondStep;
@@ -28,6 +30,8 @@ public class TestIk : MonoBehaviour
         if (Physics.Raycast(ray, out hit, rayLength, layerMask))
         {
             ikTip.transform.position = hit.point + new Vector3(0, legHeightAdjust, 0);
+            foot.transform.localRotation = Quaternion.FromToRotation(foot.transform.up, hit.normal) * foot.transform.rotation;
+            print(hit.normal);
         }
         Debug.DrawRay(ray.origin + transform.right * -2f, ray.direction * rayLength, Color.red);
     }
